6633dba19e544840161fd59644c8905197ce0821
Typson A Sánchez M <styp152@gmail.com>
2018-11-26 19:13:45 -0500

    [add] INFURA to connect to kovan, truffle-flattener to publish code in etherscan and ignore .env for local config

--- truffle.js
+++ truffle.js
@@ -1,11 +1,19 @@
-module.exports = {
-  networks: {
-    development: {
-      host: 'localhost',
-      port: 8545,
-      network_id: '*', // Match any network id
-      // https://github.com/trufflesuite/truffle/issues/271#issuecomment-341651827
-      gas: 2900000
-    }
-  }
-};
+require('dotenv').config();
+ const HDWalletProvider = require("truffle-hdwallet-provider");
+
+ module.exports = {
+   networks: {
+     development: {
+       host: "localhost",
+       port: 8545,
+       network_id: '*', // Match any network id
+       // https://github.com/trufflesuite/truffle/issues/271#issuecomment-341651827
+       gas: 2900000
+     },
+     kovan: {
+         provider: new HDWalletProvider(process.env.MNENOMIC, "https://kovan.infura.io/v3/" + process.env.INFURA_API_KEY),
+         network_id: "*",
+         gas: 5000000
+     },
+   }
+ };
